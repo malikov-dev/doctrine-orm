562b2ed538f9c6c64d232bbea639e9290b588be0
nail-php <malikov.kzn@gmail.com>
2022-01-13 18:25:43 +0300

    выборка из БД методом native query и DQL

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <div style="padding: 50px">
     <a href="/actions/create_user.php">create user</a><br><br>
     <a href="/actions/create_book.php">create book</a> - не забудь, что поле <em>title</em> должно быть уникально<br><br>
+    <a href="/actions/native_query.php">native query</a> - выборка из БД методом native query и DQL<br><br>
 </div>
