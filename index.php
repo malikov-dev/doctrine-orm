89943ed237eb304b2a0637d8d7155e0e54f0d910
nail-php <malikov.kzn@gmail.com>
2022-01-13 17:07:24 +0300

    типы опций для полей таблиц

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <div style="padding: 50px">
-    <a href="/actions/create_user.php">create user</a>
+    <a href="/actions/create_user.php">create user</a><br><br>
+    <a href="/actions/create_book.php">create book</a> - не забудь, что поле <em>title</em> должно быть уникально<br><br>
 </div>
